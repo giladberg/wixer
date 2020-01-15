92513a037f370e3883fb93298ffc76717c548226
Tal Mashiah <talmashiah@gmail.com>
2020-01-15 14:01:02 +0200

    section

--- frontend/src/pages/TemplateEdit.jsx
+++ frontend/src/pages/TemplateEdit.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from '../cmps/Title.jsx'
 
 export default class TemplateEdit extends Component {
     render() {
         return (
             <div>
                 <h1>Template edit</h1>
+                <Title/>
             </div>
         )
     }
