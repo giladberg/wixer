6863cc59b3749b23d3d3db8657938130f6c3b35b
gilad <giladberg@gmail.com>
2020-01-16 16:54:47 +0200

    aa

---
+++ frontend/src/cmps/tools/AddSection.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddSection extends Component {
+    render() {
+        return (
+            <div>
+                add section
+            </div>
+        )
+    }
+}
--- frontend/src/cmps/tools/CmpBoxMenu.jsx
+++ frontend/src/cmps/tools/CmpBoxMenu.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 export default class CmpBoxMenu extends Component {
     componentDidMount() {
-        var items = document.querySelectorAll('.circle div');
-
-        for (var i = 0, l = items.length; i < l; i++) {
+     this.setButtons()
+    }
+    setButtons=()=>{
+        const items = document.querySelectorAll('.circle div');
+        for (let i = 0, l = items.length; i < l; i++) {
             // items[i].style.left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
             // items[i].style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
             items[i].style.top=(15+i*items.length*5)+"%"
             items[i].style.left=(20+i*items.length*5)+"%"
         }
-
         document.querySelector('.menu-button').onclick = function (e) {
             e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
         }
+    }
+    onChangeMode=(status)=>{
+        this.props.onChangeMode(status)
     }
     render() {
         return (
@@ -20,10 +24,9 @@ export default class CmpBoxMenu extends Component {
                 <div className="circle">
                     <div className="icon-paint-format"></div>
                     <div className="icon-bin"></div>
-                    <div className="icon-plus"></div>
+                    <div className="icon-plus" onClick={this.onChangeMode.bind(null,'add')}></div>
                 </div>
                 <div className="menu-button icon-plus flex align-center justify-center"></div>
-
             </nav>
         )
     }
